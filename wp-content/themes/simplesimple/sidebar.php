ed6477c758ceb3f147850850b760fa289b2f3f8a
hirata3200 <k1420kei@gmail.com>
2021-11-04 11:59:51 +0900

    WordPress

--- wp-content/themes/simplesimple/sidebar.php
+++ wp-content/themes/simplesimple/sidebar.php
@@ -4,31 +4,113 @@
 <!-- sidebar.php -->
 <!-- sidebar -->
 <div id="sidebar">
+
+
 	<div class="widget">
 		<h2>Recent Posts</h2>
-		<?php $args = array( 'posts_per_page' => 3, );
+	<?php
+		
+
+
+		$args = array(
+			'posts_per_page' => 3,
+		);
 		$my_query = new WP_Query( $args );
+
+
 		if ( $my_query->have_posts() ) :  ?>
 			<ul id="sidebar-recent-posts" class="sidebar-posts">
-			<?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
-				<li class="clearfix">
-					<div class="sidebar-recent-posts-title">
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-						<p class="sidebar-date"><?php  the_time( get_option( 'date_format' ) ); ?></p>
-						<p class="sidebar-comment-num"><?php comments_popup_link( 'Comment : 0', 'Comment : 1', 'Comments : %' ); ?></p>
+		<?php
 
-					</div>
-					<p class="sidebar-thumbnail-box">
-						<a href="<?php the_permalink(); ?>">ここから</a>
-					</p>
-			</li>
-		</ul>
+			while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+
+				<?php
+
+
+
+				get_template_part( 'sidebarposts' ); ?>
+		<?php
+
+			endwhile; ?>
+			</ul>
+	<?php
+		else :
+
+
+
+			get_template_part( 'sidebarposts', 'none' );
+
+
+		endif;
+		wp_reset_postdata(); ?>
 	</div>
+	<!--  -->
 
-	if ( is_active_sidebar( 'sidebar-1' )) :
-		dynamic_sidebar('sidebar-1');
-	endif;
-	?>
+	<!--  -->
+	<div class="widget">
+		<h2>Popular Posts</h2>
+	<?php
+
+
+
+		$args = array(
+			'posts_per_page'	=> 3,
+			'orderby' 			=> 'comment_count',
+		);
+		$my_query = new WP_Query( $args );
+
+
+		if ( $my_query->have_posts() ) : ?>
+			<ul id="sidebar-recent-posts" class="sidebar-posts">
+		<?php
+
+			while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+
+				<?php
+
+
+
+				 get_template_part( 'sidebarposts' ); ?>
+
+		<?php
+
+			endwhile; ?>
+			</ul>
+	<?php
+		else :
+
+
+
+			get_template_part( 'sidebarposts', 'none' );
+
+		endif;
+		wp_reset_postdata(); ?>
+	</div>
+	<!--  -->
+
+	<!--  -->
+	<div class="widget">
+		<h2>Tag Cloud</h2>
+		<?php $args = array(
+			'smallest' 	=> 14,
+			'largest' 	=> 18,
+			'unit' 		=> 'px',
+			'number' 	=> 0,
+			'format' 	=> 'flat',
+			'taxonomy' 	=> 'post_tag',
+			'echo' 		=> true,
+		); ?>
+		<p class="tagcloud">
+			<?php wp_tag_cloud( $args ); ?>
+		</p>
+	</div>
+	
+
+<?php
+
+	if ( is_active_sidebar( 'sidebar-1' ) ) :
+		dynamic_sidebar( 'sidebar-1' );
+	endif; ?>
 </div>
 <!-- /sidebar -->
 <!-- /sidebar.php -->
